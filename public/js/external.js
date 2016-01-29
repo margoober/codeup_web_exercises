393c640220a8f9f470362b885aedb80fd8ae772a
Margot McMahon <andlmcm@Andrews-MacBook-Air.local>
2016-01-29 13:57:48 -0600

    afternoon of jan 29

--- public/js/external.js
+++ public/js/external.js
@@ -6,3 +6,30 @@ console.log(result);
 
 console.log(1 + 1);
 console.log("Hello from EXTERNAL JAVASCRIPT!".toUpperCase());
+
+<!--conditionals -->
+
+
+if (true && (false || true)) {
+	console.log("true");
+}
+
+if (name === "Margot") {
+	console.log("My name is " + "Margot!");
+}
+var color = "red";
+
+if (color == "red") {
+	console.log("It is red.");
+} else if (color == "green") {
+	console.log("It is green.");
+} else {
+	console.log("It's not a real color, then.");
+}
+
+
+
+
+var color1 = "rash";
+
+console.log((color1 == "red")? "it was red" : "it warn't red.");
