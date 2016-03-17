abda53af291e0e7d99e5d180d89df5e5127ee9f6
Margot McMahon <amcmahon@nes.ru>
2016-03-17 14:56:33 -0500

    park_migration mostly ready to go!

--- public/park_migration.php
+++ public/park_migration.php
@@ -9,3 +9,23 @@ require_once('db_connect.php');
 /*verify connection*/
 echo 'connection check for ' . DB_USER . ' to database ' . DB_NAME . PHP_EOL;
 echo $connection->getAttribute(PDO::ATTR_CONNECTION_STATUS) . PHP_EOL;
+
+/*use exec() to DROP TABLE IF EXISTS national_parks*/
+$dropTable = <<<QUERY
+	DROP TABLE IF EXISTS national_parks
+QUERY;
+$connection->exec($dropTable);
+
+/*use exec() to CREATE national_parks*/
+$createTable = <<<QUERY
+	CREATE TABLE national_parks(
+		park_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		name VARCHAR(100) NOT NULL,
+		location VARCHAR(100) NOT NULL,
+		date_est YEAR(4) NOT NULL,
+		acreage DOUBLE NOT NULL,
+		PRIMARY KEY(park_id)
+		)
+QUERY;
+
+$connection->exec($createTable);
